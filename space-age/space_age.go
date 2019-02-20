54fdaaafe0b8923d77c82bbff50360feeaf992e0
Anaru Herbert <anaruherbert@gmail.com>
2019-02-21 07:11:25 +1000

    feat: complete space age implementation

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -1,7 +1,7 @@
 package space
 
-// Age takes a string planet and seconds and does some arithmetic
-// to calculate age based on that planets concept of a year relative to an earth year
+// Age takes a string planet and float seconds and calculates
+// age in that planets years relative to an earth year
 func Age(seconds float64, planet string) float64 {
 	//earth year in seconds
 	const earthYear float64 = 31557600
@@ -10,6 +10,18 @@ func Age(seconds float64, planet string) float64 {
 	switch planet {
 	case "Mercury":
 		relativeAgeBasedOnEarthYears = relativeAgeBasedOnEarthYears / 0.2408467
+	case "Venus":
+		relativeAgeBasedOnEarthYears = relativeAgeBasedOnEarthYears / 0.61519726
+	case "Mars":
+		relativeAgeBasedOnEarthYears = relativeAgeBasedOnEarthYears / 1.8808158
+	case "Jupiter":
+		relativeAgeBasedOnEarthYears = relativeAgeBasedOnEarthYears / 11.862615
+	case "Saturn":
+		relativeAgeBasedOnEarthYears = relativeAgeBasedOnEarthYears / 29.447498
+	case "Uranus":
+		relativeAgeBasedOnEarthYears = relativeAgeBasedOnEarthYears / 84.016846
+	case "Neptune":
+		relativeAgeBasedOnEarthYears = relativeAgeBasedOnEarthYears / 164.79132
 	}
 
 	return relativeAgeBasedOnEarthYears
