299cb4ec141f57dbe15f5e03852d47d1f216300d
Anaru Herbert <anaruherbert@gmail.com>
2019-02-20 20:56:07 +1000

    prog: side project space age

---
+++ space-age/space_age.go
@@ -0,0 +1,7 @@
+package space
+
+// Age takes a string planet and seconds and does some arithmetic
+// to calculate age based on that planets cocncept of a year
+func Age(seconds float64, planet string) {
+
+}
