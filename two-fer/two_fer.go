5d8bc092144b5b8db8212a1271882a626c792000
Anaru Herbert <anaruherbert@gmail.com>
2019-02-21 10:11:18 +1000

    feat: stringbuilder implementation for twofer excercise.

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,12 +1,22 @@
 // Package twofer implements function to return statements based on a name
 package twofer
 
+import "strings"
+
 // ShareWith returns statement 'one for {name}, one for me'
 // if no name is found then 'you' is substituted in.
 func ShareWith(name string) string {
+	var sb strings.Builder
+
+	//check for empty string case and replace with default statement
 	if name == "" {
 		name = "you"
 	}
 
-	return "One for " + name + ", one for me."
+	//build up the string we want to return and return it
+	sb.WriteString("One for ")
+	sb.WriteString(name)
+	sb.WriteString(", one for me.")
+
+	return sb.String()
 }
